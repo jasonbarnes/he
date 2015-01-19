f5210e5ff4b3646f156faa1db91eaf4e2c5a5862
Jason Barnes <jason@pc12core20.onl>
2015-01-19 15:06:19 -0600

    Added more to floating point representation.

--- he_exp/Test_General.cpp
+++ he_exp/Test_General.cpp
@@ -29,6 +29,7 @@
 #include <cstdio>
 #include <deque>
 #include <cmath>
+#include <cstring>
 
 #include <string>
 #include <iostream>
@@ -62,7 +63,7 @@
 //#define HE_DR_DESTRUCT_ON
 
 //Define the next line to turn HElib on.
-#define HE_ON
+//#define HE_ON
 //Define this line to ignore HElib and Plaintext, and output the value as if doubles were used.
 //#define HE_USE_TRACK_VAL
 typedef int P_int;
@@ -82,10 +83,13 @@ class HE_float{
 	#endif
 	deque<unsigned long> p_mant;
 	deque<unsigned long> p_expo;
+	deque<unsigned long> final_mant;
+	deque<unsigned long> final_expo;
 	double orig_value;
 	double final_value;
 	double track_value;
-	int base;
+	unsigned long base;
+	int bias;
 	int mant_bits;
 	int expo_bits;
 	FHESecKey *HE_seckey;
@@ -101,8 +105,9 @@ class HE_float{
 	void encrypt();
 	void decrypt();
 	double extract();
+	void negate();
 	void operator=(const HE_float &in);
-	void operator+-(const HE_float &in);
+	void operator+=(const HE_float &in);
 	void operator-=(const HE_float &in);
 	void operator*=(const HE_float &in);
 	void operator/=(const HE_float &in);
@@ -123,6 +128,8 @@ HE_float::HE_float(){
 	this->base = HE_BASE;
 	this->mant_bits = HE_MANT_BITS;
 	this->expo_bits = HE_EXPO_BITS;
+	this->bias = (int)pow(2.0, this->expo_bits);
+	this->bias = (this->bias >> 1) - 1;
 	this->HE_seckey = NULL;
 	this->HE_pubkey = NULL;
 	this->HE_ea = NULL;
@@ -136,6 +143,8 @@ HE_float::HE_float(FHESecKey *in_seckey, const FHEPubKey *in_pubkey, EncryptedAr
 	this->base = HE_BASE;
 	this->mant_bits = HE_MANT_BITS;
 	this->expo_bits = HE_EXPO_BITS;
+	this->bias = (int)pow(2.0, this->expo_bits);
+	this->bias = (this->bias >> 1) - 1;
 	this->HE_seckey = in_seckey;
 	this->HE_pubkey = in_pubkey;
 	this->HE_ea = in_ea;
@@ -143,19 +152,95 @@ HE_float::HE_float(FHESecKey *in_seckey, const FHEPubKey *in_pubkey, EncryptedAr
 	this->encode();
 }
 
+//This function takes the value in orig_value and transforms it into a bitwise representation
+//Stored in p_mant and p_expo
 void HE_float::encode(){
+	this->orig_value = abs(this->orig_value); //This scheme is only proved for positive
+						  //values.  This may change later.
+
+	float fl_value = (float)this->orig_value; //We lose some precision here, but it's
+						  //easier than working with doubles during
+						  //bit conversions...
+	unsigned int bits;
+	deque<unsigned long> temp_mant;
+	assert(sizeof(float)==sizeof(int));//Just to make sure this isnt an odd OS/compiler.
+	memcpy(&bits, &fl_value, sizeof(float));
+	//The variable "bits" containts the bits of fl_value.  Now, we'll mask them to extract
+	//the exponent and mantissa
+	int i,j;
+	this->p_mant.clear();
+	this->p_expo.clear();
+	temp_mant.clear();
+	for(i=0 ; i < this->mant_bits; i++){
+		temp_mant.push_back(((1<<i)&bits)>>i);
+		this->p_mant.push_back(0);
+	}
+	j=0;
+	this->p_mant = temp_mant;
+	//We also have to push back some additional bits.  We have HE_MANT_BITS functional
+	//bits that carry info, along with a top carry bit (the understood '1' in most
+	//floating point reps) and enough extra bits on top to account for excess mult.
+	//room.
+	this->p_mant.push_back(1);
+	for(i=0 ; i < (this->mant_bits+1) ; i++){
+		this->p_mant.push_back(0);
+	}
+	bits = bits >> this->mant_bits;
+	for(i=0 ; i < (int)pow(2.0, (double)this->expo_bits); i++){
+		if((unsigned int)i==bits){
+			this->p_expo.push_back(1);
+		}
+		else{
+			this->p_expo.push_back(0);
+		}
+	}
+	//Now we've encoded the mantissa and exponent into p_mant and p_expo.  We just
+	//have to encrypt, and the number is ready.
+	this->encrypt();
 	return;
 }
 
 void HE_float::decode(){
+	this->decrypt();
+	//The decrypted bits are now in final_mant and final_expo.
+	//We can't cast directly back to floats or doubles, we have to do this the hard way.
+	this->final_value=0.0;
+	int i, j;
+	j=0;
+	//We have to get rid of the extra multiply space first:
+	for(i=0 ; i < (this->mant_bits)+1 ; i++){
+		this->final_mant.pop_back();
+	}
+	for(i=this->final_mant.size()-1; i>=0; i--){
+		this->final_value+=final_mant[i]*pow(2.0, (double)j);
+		j--;
+	}
+	for(i=0 ; i < final_expo.size() ; i++){
+		if(p_expo[i] == 1){
+			break;
+		}
+	}
+	int temp_expo = i-this->bias;
+	this->final_value*=pow(2.0, (double)temp_expo);
 	return;
 }
 
 void HE_float::encrypt(){
+	#ifdef HE_ON
+	//do encryption stuff
+	#endif
 	return;
 }
 
 void HE_float::decrypt(){
+	#ifdef HE_ON
+	//do decryption stuff
+	#else
+	//Decryption is off, so we'll just transfer p_mant and p_expo to the final variables:
+	int i;
+	this->final_mant = this->p_mant;
+	this->final_expo = this->p_expo;
+	#endif
 	return;
 }
 
@@ -165,19 +250,270 @@ double HE_float::extract(){
 }
 
 void HE_float::operator=(const HE_float &in){
+	#ifdef HE_ON
+	//Ciphertext equivalence code
+	#endif
+	//Now, we do the same for the plaintext and other variables;
+	this->p_mant = in.p_mant;
+	this->p_expo = in.p_expo;
+	this->final_mant = in.final_mant;
+	this->final_expo = in.final_expo;
+	this->orig_value = in.orig_value;
+	this->final_value = in.final_value;
+	this->track_value = in.track_value;
+	this->base = in.base;
+	this->bias = in.bias;
+	this->mant_bits = in.mant_bits;
+	this->expo_bits = in.expo_bits;
+	this->HE_seckey = in.HE_seckey;
+	this->HE_pubkey = in.HE_pubkey;
+	this->HE_ea = in.HE_ea;
+	return;
+}
+
+deque<unsigned long> rightShiftP(deque<unsigned long> a, int n){
+	int i;
+	deque<unsigned long> ret = a;
+	for(i=0 ; i < n ; i++){
+		ret.pop_front();
+		ret.push_back(0);
+	}
+	return ret;
+}
+deque<unsigned long> leftShiftP(deque<unsigned long> a, int n){
+	int i;
+	deque<unsigned long> ret = a;
+	for(i=0 ; i < n ; i++){
+		ret.pop_back();
+		ret.push_front(0);
+	}
+	return ret;
+}
+
+#ifdef HE_ON
+deque<deque<Ctxt *> > generateMantOptionsA(deque<Ctxt *> a){
+	//Takes a Ctxt array and returns all possible shift options
+	//Does right shift first, then nothing.
+	deque<deque<Ctxt *> > ret;
+	return ret;
+}
+deque<deque<Ctxt *> > generateMantOptionsB(deque<Ctxt *> a){
+	//Takes a Ctxt array and returns all possible shift options
+	//Does nothing first, then right shift.
+	deque<deque<Ctxt *> > ret;
+	return ret;
+}
+#endif
+
+deque<deque<unsigned long> > generatePMantOptionsA(deque<unsigned long> a){
+	//Like generateMantOptions, but for plaintext
+	deque<deque<unsigned long> > ret;
+	unsigned long i;
+	deque<unsigned long> temp;
+	for(i=0 ; i < a.size() ; i++){
+		temp = rightShiftP(a, i);
+		ret.push_back(temp);
+	}
+	for(i=1 ; i < a.size() ; i++){
+		temp = a;
+		ret.push_back(a);
+	}
+	return ret;
+}
+
+deque<deque<unsigned long> > generatePMantOptionsB(deque<unsigned long> a){
+	//Like generateMantOptions, but for plaintext
+	deque<deque<unsigned long> > ret;
+	unsigned long i;
+	deque<unsigned long> temp;
+	for(i=0 ; i < a.size() ; i++){
+		temp = a;
+		ret.push_back(temp);
+	}
+	for(i=1 ; i < a.size() ; i++){
+		temp = rightShiftP(a, i);
+		ret.push_back(a);
+	}
+	return ret;
+}
+
+#ifdef HE_ON
+deque<deque<Ctxt*> > generateExpoOptionsA(deque<Ctxt *> a){
+	//Stuff
+}
+deque<deque<Ctxt*> > generateExpoOptionsB(deque<Ctxt *> a){
+	//Stuff
+}
+#endif
+
+deque<deque<unsigned long> > generatePExpoOptionsA(deque<unsigned long> a, int mant_size){
+	deque<deque<unsigned long> > ret;
+	int i;
+	deque<unsigned long> temp;
+	for(i=0 ; i < mant_size ; i++){
+		temp = a;
+		ret.push_back(leftShiftP(a, i));
+	}
+	for(i=1 ; i < mant_size ; i++){
+		temp = a;
+		ret.push_back(temp);
+	}
+	return ret;
+}
+
+deque<deque<unsigned long> > generatePExpoOptionsB(deque<unsigned long> a, int mant_size){
+	deque<deque<unsigned long> > ret;
+	int i;
+	deque<unsigned long> temp;
+	for(i=0 ; i < mant_size ; i++){
+		temp = a;
+		ret.push_back(temp);
+	}
+	for(i=1 ; i < mant_size ; i++){
+		temp = a;
+		ret.push_back(leftShiftP(a, i));
+	}
+	return ret;
+}
+
+deque<unsigned long> generatePSelectArray(deque<deque<unsigned long> > a, deque<deque<unsigned long> >b){
+	deque<deque<unsigned long> > c;
+	deque<unsigned long> temp;
+	int i,j;
+
+	for(i=0 ;i < a.size() ; i++){
+		c.push_back(a[i]);
+		for(j=0 ; j < a[0].size() ; i++){
+			c[i][j] &= b[i][j];
+		}
+	}
+	temp.clear();
+	unsigned long value;
+	for(i=0 ; i < c.size() ; i++){
+		value = 0;
+		for(j=0 ; j < c[0].size() ; j++){
+			value += c[i][j];
+		}
+		temp.push_back(value);
+	}
+	return temp;
+}
+
+deque<unsigned long> p_select(deque<unsigned long> select, deque<deque<unsigned long> > options){
+	int i,j;
+	deque<deque<unsigned long> > select_result;
+	deque<unsigned long> final_result;
+	for(i=0 ; i < select.size() ; i++){
+		select_result.push_back(options[i]);
+		for(j=0 ; j < select_result[i].size() ; j++){
+			select_result[i][j] *= select[i];
+		}
+	}
+	final_result = select_result[i];
+	for(i=1 ; i < select_result.size(); i++){
+		for(j=0 ; j < select_result[i].size(); j++){
+			final_result[j] += select_result[i][j];
+		}
+	}
+	return final_result;
+}
+
+void additiveRescale(HE_float *a, HE_float *b){
+	//Takes 2 HE_floats, and adjusts them so that their exponents are equivalent
+	//If exponents are more than 23 apart, it won't work
+	#ifdef HE_ON
+	#endif
+	deque<deque<unsigned long> > p_expo_options_a = generatePExpoOptionsA(a->p_expo, a->p_mant.size());
+	deque<deque<unsigned long> > p_expo_options_b = generatePExpoOptionsB(b->p_expo, a->p_mant.size());
+	deque<deque<unsigned long> > p_mant_options_a = generatePMantOptionsA(a->p_mant);
+	deque<deque<unsigned long> > p_mant_options_b = generatePMantOptionsB(b->p_mant);
+	deque<unsigned long> p_select_array = generatePSelectArray(p_expo_options_a, p_expo_options_b);
+//	unsigned long p_max_select = generatePMaxSelect(p_select_array);
+	a->p_expo = p_select(p_select_array, p_expo_options_a);
+	b->p_expo = p_select(p_select_array, p_expo_options_b);
+	a->p_mant = p_select(p_select_array, p_mant_options_a);
+	b->p_mant = p_select(p_select_array, p_mant_options_b);
 	return;
 }
 
 void HE_float::operator+=(const HE_float &in){
+	HE_float a = in;
+	additiveRescale(this, &a);
+	#ifdef HE_ON
+	//Ciphertext code
+	#else
+	//This is the plaintext-only version.
+	//Exponents are the same, so we just have to do an add:
+	int i;
+	for(i=0 ; i < this->p_mant.size() ; i++){
+		this->p_mant[i] += a.p_mant[i];
+		this->p_mant[i] = this->p_mant[i] % this->base;
+	}
+	#endif
+	return;
+}
+
+void HE_float::negate(){
+	//Here, we negate each digit.
+	#ifdef HE_ON
+	#endif
+	int i;
+	for(i=0 ; i < this->p_mant.size() ; i++){
+		this->p_mant[i] = base - this->p_mant[i];
+	}
 	return;
 }
 
 void HE_float::operator-=(const HE_float &in){
+	HE_float a = in;
+	a.negate();
+	(*this)+=a;
 	return;
+}
+
+deque<unsigned long> multiplyExpoP(deque<unsigned long> a, deque<unsigned long>b){
+	deque<deque<unsigned long> > expo_options;
+	int i,j;
+	deque<unsigned long> temp;
+	for(i=1 ; i < a.size() ; i++){
+		expo_options.push_back(leftShiftP(a, i));
+		temp = rightShiftP(b, i-1);
+		for(j=0 ; j < expo_options[i-1].size() ; j++){
+			expo_options[i][j] *= temp[0];
+		}
+	}
+	temp.clear();
+	temp = expo_options[0];
+	for(i=1 ; i < expo_options[0].size() ; i++){
+		for(j=0 ; j < temp.size() ; j++){
+			temp[j] += expo_options[i][j];
+		}
+	}
+	return temp;
 }
 
 
 void HE_float::operator*=(const HE_float &in){
+	HE_float a = in;
+	#ifdef HE_ON
+	#else
+	deque<unsigned long> res_expo_p = multiplyExpoP(this->p_expo, a.p_expo);
+	deque<unsigned long> res;
+	int i,j;
+	for(i=0 ; i < a.p_mant.size() ; i++){
+		res.push_back(0);
+	}
+	deque<unsigned long> temp;
+	for(i=0 ; i < a.p_mant.size() ; i++){
+		temp = leftShiftP(a.p_mant, i);
+		for(j=0 ; j< temp.size() ; j++){
+			temp[j] *= this->p_mant[j];
+			res[j] += temp[j];
+		}
+	}
+	this->p_mant = res;
+	this->p_expo = res_expo_p;
+	#endif
 	return;
 }
 
@@ -294,6 +630,7 @@ void TestIt()
 #endif
 //CSGD Start
 
+#ifdef HE_ON
 FHESecKey *HE_seckey;
 const FHEPubKey *HE_pubkey;
 EncryptedArray *HE_ea;
@@ -303,7 +640,10 @@ HE_ea=&ea;
 
 HE_float Xa;
 HE_float Xb(HE_seckey, HE_pubkey, HE_ea, 1.7);
-
+#endif
+HE_float Xa;
+HE_float Xb(NULL, NULL, NULL, 1.7);
+printf("%f\n", Xb.extract());
 //CSGD End
 
 
